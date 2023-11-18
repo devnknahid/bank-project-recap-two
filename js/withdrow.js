61412c072e55e09610d6d32c2e32a087d3081d9d
devnknahid <dev.nknahid39@gmail.com>
2023-11-19 00:21:56 +0600

    get withdrow value and set withdrow ammount

--- js/withdrow.js
+++ js/withdrow.js
@@ -1,8 +1,24 @@
 document.getElementById('withdrow_btn').addEventListener('click', function () {
+
+   // withdrow_field
    const withdrow_field = document.getElementById('withdrow_field');
    const withdrow_field_string = withdrow_field.value
    const withdrow_field_float = parseFloat(withdrow_field_string);
-   
 
+   withdrow_field.value = '';
+   if (isNaN(withdrow_field_float)) {
+      alert('vai leave some value');
+      return;
+   }
+
+   // withdrow_number
+   const withdrow_number = document.getElementById('withdrow_number');
+   const withdrow_number_string = withdrow_number.innerText;
+   const withdrow_number_float = parseFloat(withdrow_number_string);
+
+   const add_withdrow_number = withdrow_number_float + withdrow_field_float;
+   withdrow_number.innerText = add_withdrow_number;
+
+   
 
 })
