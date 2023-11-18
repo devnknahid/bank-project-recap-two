05700a1e361ccc640e55441354683e39f4d2ccdb
devnknahid <dev.nknahid39@gmail.com>
2023-11-19 00:11:34 +0600

    catch withdrow field

---
+++ js/withdrow.js
@@ -0,0 +1,8 @@
+document.getElementById('withdrow_btn').addEventListener('click', function () {
+   const withdrow_field = document.getElementById('withdrow_field');
+   const withdrow_field_string = withdrow_field.value
+   const withdrow_field_float = parseFloat(withdrow_field_string);
+   
+
+
+})
